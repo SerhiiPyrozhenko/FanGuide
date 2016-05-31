dbcc115bd1077cfd270f31e75826f24fcc16ee84
SerhiiPyrozhenko <Pirogenkoss@ukr.net>
2016-06-01 01:42:16 +0300

    Update Form4.cs

--- Form4.cs
+++ Form4.cs
@@ -1,20 +1,48 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OOP_FanGuide
 {
+    /// <summary>
+    /// Форма добавления спортсмена(боксер)
+    /// </summary>
+    [Serializable]
     public partial class Form4 : Form
     {
+        List<Boxer> lst = new List<Boxer>();
+
         public Form4()
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = Owner as Form1;
+            
+            string name = bName.Text;
+            string surName = bSurname.Text;
+            string nationality = bNationality.Text;
+            string age = bAge.Text;
+            string careerDuration = bYearsInSport.Text;
+            string height = bHeight.Text;
+            string weight = bWeight.Text;
+            string medals = bMedals.Text;
+            string knockouts = bKnockout.Text;
+            string wins = bWins.Text;
+            string losses = bLosses.Text;
+
+            Boxer cache = new Boxer(name, surName, nationality, age, height, weight, careerDuration, medals, wins, losses, knockouts);
+
+            //добавление в список боксеров
+            f1.listB.Add(cache);
+
+            //обновление таблицы через привязку
+            f1.UpdateDGV();
+
+            Close();
+        }
     }
 }
+
