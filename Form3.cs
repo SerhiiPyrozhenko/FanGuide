c27f497e2dbeb81af70d5ad550dd348baaefb5fa
SerhiiPyrozhenko <Pirogenkoss@ukr.net>
2016-06-01 01:41:21 +0300

    Update Form3.cs

--- Form3.cs
+++ Form3.cs
@@ -1,35 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OOP_FanGuide
 {
+    /// <summary>
+    /// Форма добавления спортсмена(тенисист).
+    /// </summary>
+    [Serializable]
     public partial class Form3 : Form
     {
+        List<TennisPlayer> lst = new List<TennisPlayer>();
+
         public Form3()
         {
             InitializeComponent();
         }
 
-        private void Form3_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            string sport = "Tennis";
+            Form1 f1 = Owner as Form1;
+
             string name = tName.Text;
             string surName = tSurname.Text;
             string nationality = tNationality.Text;
@@ -37,7 +28,6 @@ namespace OOP_FanGuide
             string careerDuration = tYearsInSport.Text;
             string height = tHeight.Text;
             string weight = tWeight.Text;
-            string megals = tMedals.Text;
             string seriesOfWin = tSeries.Text;
             string attack = tAttack.Text;
             string defense = tDefence.Text;
@@ -45,8 +35,15 @@ namespace OOP_FanGuide
             string losses = tLosses.Text;
 
             TennisPlayer cache = new TennisPlayer(name, surName, nationality, age, height, weight, careerDuration, seriesOfWin, defense, attack,wins,losses);
+            
+            //добавление в список тенисиста
+            f1.listT.Add(cache);
 
-            //dataGridView2.Rows.Add(name, surName, nationality, age, height, weight, careerDuration, seriesOfWin, defense, attack, wins, losses);
+            //обновление таблицы через привязку
+            f1.UpdateDGV();
+
+            Close();
         }
     }
 }
+
